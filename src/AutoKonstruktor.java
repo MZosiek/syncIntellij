4ede15191471379d1dc11e33cd525dd2da43b31d
Zosiek <zosiek.techniczne@gmail.com>
2021-12-20 07:36:05 +0100

    tablice i objekty

---
+++ src/AutoKonstruktor.java
@@ -0,0 +1,40 @@
+import java.util.Calendar;
+
+public class AutoKonstruktor {
+
+    public String marka;
+    public String model;
+    public int przebieg;
+    public int rok;
+    public double cena;
+
+    int year = Calendar.getInstance().get(Calendar.YEAR);
+
+
+    public  AutoKonstruktor( String marka, String model, int przebieg, int rok, double cena){
+
+        this.marka = marka;
+        this.model = model;
+        this.przebieg = przebieg;
+        this.rok=rok;
+        this.cena = cena;
+
+        System.out.println("Marka " + marka);
+        System.out.println("Model " + model);
+        System.out.println("Przebieg "+ przebieg);
+        System.out.println("Rok "+ rok);
+        System.out.println("cena " + cena);
+
+    }
+
+    public double nowy(){
+
+        if(przebieg !=0 && year-rok<=15 ){
+            cena= cena*0.9;
+        }
+        return cena;
+    }
+
+
+
+}
---
+++ src/CarModelsKonstruktor.java
@@ -0,0 +1,21 @@
+public class CarModelsKonstruktor {
+    public static void main(String[] args) {
+
+
+        AutoKonstruktor audi = new AutoKonstruktor("Audi", "S4", 1000, 2020, 10000.00);
+
+
+        AutoKonstruktor bmw = new AutoKonstruktor("BMW" , "model 5", 0,2020,20000.00);
+
+        audi.cena = audi.nowy();
+
+        bmw.cena = bmw.nowy();
+
+
+
+
+        System.out.println("audi cena " + audi.cena);
+
+
+    }
+}
